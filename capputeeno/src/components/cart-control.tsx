d960b947c932ff256b505aa30df442a0c62bda23
vinicius-je <vinicius.je99@gmail.com>
2023-06-04 10:41:25 -0300

    feat: header

---
+++ capputeeno/src/components/cart-control.tsx
@@ -0,0 +1,30 @@
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { CartIcon } from "@/icons/cart-icon";
+import styled from "styled-components";
+
+const Container = styled.div`
+    position: relative;
+`
+const CartCount = styled.span`
+    width: 17px;
+    height: 17px;
+    border-radius: 100%;
+    padding: 0 5px;
+    font-size: 10px;
+
+    background-color: var(--delete-color);
+    color: white;
+
+    margin-left: -10px;
+`
+
+export function CartControl(){
+    const { value } = useLocalStorage('item', []);
+
+    return(
+        <Container>
+            <CartIcon/>
+            {value.length != null && <CartCount>{value.length}</CartCount>}
+        </Container>
+    )
+}
