5f6c974e357d69858d7b8e2754a29c36339aa34d
vinicius-je <vinicius.je99@gmail.com>
2023-06-08 22:22:56 -0300

    feat: add filtering and listing products

--- capputeeno/src/components/products-list.tsx
+++ capputeeno/src/components/products-list.tsx
@@ -1,13 +1,28 @@
 "use client"
-import { useProducts } from "@/hooks/userProducts"
+import { useProducts } from "@/hooks/useProducts"
+import { ProductCard } from "./product-card";
+import styled from "styled-components";
 
-interface ProductsListProps {
+const ListContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 256px);
+    grid-gap: 32px;
+    max-width: 100%;
+    margin-top: 32px;
+`
 
-}
-export function ProductsList(props : ProductsListProps){
+export function ProductsList(){
     const { data } = useProducts();
-    console.log(data)
+
     return(
-        <></>
+        <ListContainer>
+            {data?.map(product => 
+                <ProductCard 
+                    key={product.id}
+                    image={product.image_url}
+                    title={product.name}
+                    price={product.price_in_cents}/>
+            )}
+        </ListContainer>
     )
 }
---
+++ capputeeno/src/hooks/useProducts.tsx
@@ -0,0 +1,31 @@
+import { ProductsFetchResponse } from "@/types/products-response";
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosPromise } from "axios";
+import { useFilter } from "./useFilter";
+import { mountQuery } from "@/utils/graphql-filters";
+import { useDeferredValue } from "react";
+
+//gerante que será uma string
+const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+
+const fetcher = (query: string): AxiosPromise<ProductsFetchResponse> => {
+    return axios.post(API_URL, { query })
+}
+
+export function useProducts(){
+    const { type, priority, search } = useFilter()
+    //Permite o filtro de produtos apenas ao final do input
+    const searchDeferred = useDeferredValue(search)
+    const query = mountQuery(type, priority)
+    const { data } = useQuery({
+        queryFn: () => fetcher(query),
+        queryKey: ['products', type, priority]
+    })
+
+    const products =  data?.data?.data?.allProducts
+    const filteredProducts = products?.filter(product => product.name.toLowerCase().includes(searchDeferred.toLowerCase()))
+
+    return{
+        data: filteredProducts
+    }
+}
